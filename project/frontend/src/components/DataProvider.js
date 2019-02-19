9566ac9e66dffd7ab3fbbf05ac33c936b5c6855d
Alvaro Reis Neto <ait@certi.org.br>
2019-02-19 15:44:41 -0400

    showing data from api

--- project/frontend/src/components/DataProvider.js
+++ project/frontend/src/components/DataProvider.js
@@ -15,10 +15,10 @@ class DataProvider extends Component {
 
   componentDidMount() {
     fetch(this.props.endpoint)
-      .then(response => {
-        if (response.status !== 200 || response.status !== 404) {
+      .then(response => {        
+        if (response.status !== 200) {
           return this.setState({ placeholder: "Something went wrong" });
-        }
+        }                
         return response.json();
       })
       .then(data => this.setState({ data: data, loaded: true }));
